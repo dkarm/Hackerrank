56e4126d6e72bdd3d60d909f3bc3e0d6672191d0
Dipti Karmarkar <diptikarmarkar@Diptis-MacBook-Pro.local>
2017-05-31 14:22:43 -0500

    finished dfs

--- CrackingCodingInterview/DFSConnectedCellGraph.py
+++ CrackingCodingInterview/DFSConnectedCellGraph.py
@@ -1,56 +1,42 @@
 def get_biggest_region(grid):
-    # visited_row = [0]*len(grid)
-    # visited_row_col = [visited_row]*len(grid[0])
     n = len(grid)
     m = len(grid[0])
     visited_row_col = [[0 for x in range(m)] for y in range(n)]
-
-    print 'original', visited_row_col
-
     max_count = 0
 
     for idx_row, point in enumerate(grid):
         for idx_col, point_col in enumerate(grid[idx_row]):
-            print "current position", idx_row, idx_col, visited_row_col[idx_row][idx_col]
-            if visited_row_col[idx_row][idx_col] ==0:
+            if visited_row_col[idx_row][idx_col] == 0:
                 cur_count = 0
                 stack_to_look = []
                 stack_to_look.append([idx_row, idx_col])
-                while len(stack_to_look) >0:
-                    print 'current stack', stack_to_look
+                while len(stack_to_look) > 0:
                     to_look = stack_to_look.pop()
                     row = to_look[0]
                     col = to_look[1]
-                    visited_row_col[row][col] = 1
-                    print 'to_look', to_look, row, col, 'visited row col', visited_row_col
-                    print 'in grid', grid[row][col]
-                    if grid[row][col] == 1:
-                        print 'yes cound!'
-                        # increment count
-                        print 'current_count', cur_count
+                    if visited_row_col[row][col] == 0 and grid[row][col] == 1:
+                        visited_row_col[row][col] = 1
                         cur_count += 1
-                        # add any surrounding members to stack
-                        if idx_row + 1 < n:
-                            stack_to_look.append([idx_row + 1, idx_col])
-                            if idx_col - 1 >= 0:
-                                stack_to_look.append([idx_row + 1, idx_col - 1])
-                            if idx_col + 1 < m:
-                                stack_to_look.append([idx_row + 1, idx_col + 1])
-                        if idx_col + 1 < m:
-                            stack_to_look.append([idx_row , idx_col + 1])
-                            if idx_row - 1 >=0:
-                                stack_to_look.append([idx_row - 1, idx_col + 1])
-                        if idx_row - 1 >=0:
-                            stack_to_look.append([idx_row - 1, idx_col])
-                            if idx_col -1 >=0:
-                                stack_to_look.append([idx_row - 1, idx_col -1])
-                        if idx_col - 1 >=0:
-                            stack_to_look.append([idx_row, idx_col -1])
-                        print 'stack after adding', stack_to_look
-                    break
+                        if row + 1 < n and visited_row_col[row + 1][col] == 0:
+                            stack_to_look.append([row + 1, col])
+                        if row + 1 < n and col - 1 >= 0 and visited_row_col[row + 1][col - 1] == 0:
+                            stack_to_look.append([row + 1, col - 1])
+                        if row + 1 < n and col + 1 < m and visited_row_col[row + 1][col + 1] == 0:
+                            stack_to_look.append([row + 1, col + 1])
+                        if col + 1 < m and visited_row_col[row][col + 1] == 0:
+                            stack_to_look.append([row, col + 1])
+                        if col + 1 < m and row - 1 >= 0 and visited_row_col[row - 1][col + 1] == 0:
+                            stack_to_look.append([row - 1, col + 1])
+                        if row - 1 >= 0 and visited_row_col[row - 1][col] == 0:
+                            stack_to_look.append([row - 1, col])
+                        if row - 1 >= 0 and col - 1 >= 0 and visited_row_col[row - 1][col - 1] == 0:
+                            stack_to_look.append([row - 1, col - 1])
+                        if col - 1 >= 0 and visited_row_col[row][col - 1] == 0:
+                            stack_to_look.append([row, col - 1])
                 if cur_count > max_count:
                     max_count = cur_count
     return max_count
+
 
 n = int(raw_input().strip())
 m = int(raw_input().strip())
